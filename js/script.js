3bfd07bd9c7d4549bf0f2ea5122e11431b42f947
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-11-08 21:53:24 +0100

    snack-2 done

--- js/script.js
+++ js/script.js
@@ -21,3 +21,46 @@ console.log(giulietta, francesca, flavia);
 
 (giulietta.peso > francesca.peso && giulietta.peso > flavia.peso) ? console.log(giulietta) : (francesca.peso > flavia.peso) ? console.log(francesca) : console.log(flavia);
 
+
+// SNACK 2
+const footballTeams = [
+    {
+        nome : "riverplate",
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome : "monzone",
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome : "forlimpopoli",
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+]
+console.log(footballTeams);
+
+footballTeams.forEach(curTeam => {
+    curTeam.puntiFatti = getRndInteger(60, 100);
+    curTeam.falliSubiti = getRndInteger(180, 300);
+});
+
+const updatedFootballTeams = [];
+
+footballTeams.forEach(curTeam => {
+    const {nome, falliSubiti} = curTeam;
+    curTeam = {nome, falliSubiti};
+    updatedFootballTeams.push(curTeam);
+});
+
+console.log(updatedFootballTeams);
+
+
+// SNACK 3
+
+// FUNCTIONS
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
